77b7986d2eb4f7b1040a8dc1eff29cc2576fd761
Sung Won Chun <s1chun3636@gmail.com>
2017-02-23 02:15:55 -0800

    fixes portrait mode, sets up test texts

--- app/src/main/java/com/example/sungwon/messageinabottle/MainActivity.java
+++ app/src/main/java/com/example/sungwon/messageinabottle/MainActivity.java
@@ -5,9 +5,13 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView mBottomNav;
+    TextView mTestText;
+    FrameLayout mContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void setupView() {
         mBottomNav = (BottomNavigationView)findViewById(R.id.bottom_navigation);
+        mTestText = (TextView)findViewById(R.id.testText);
+        mContainer = (FrameLayout)findViewById(R.id.fragment_container);
         BottomNavigationViewHelper.disableShiftMode(mBottomNav);
         mBottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -25,15 +31,19 @@ public class MainActivity extends AppCompatActivity {
                 switch (item.getItemId()){
                     case R.id.action_main_menu:
                         mBottomNav.setItemBackgroundResource(R.color.orange);
+                        mTestText.setText("Main Menu Selected!!!");
                         break;
                     case R.id.action_memory:
                         mBottomNav.setItemBackgroundResource(R.color.gray);
+                        mTestText.setText("Memory Selected!!");
                         break;
                     case R.id.action_send:
                         mBottomNav.setItemBackgroundResource(R.color.colorPrimary);
+                        mTestText.setText("Send Selected!");
                         break;
                     case R.id.action_setting:
                         mBottomNav.setItemBackgroundResource(R.color.gray2);
+                        mTestText.setText("Boring ol' Setting selected.");
                         break;
                 }
                 return true;
