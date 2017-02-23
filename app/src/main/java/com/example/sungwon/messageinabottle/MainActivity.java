abbcd1abf16967e8caef0efbd8a59e846d37e177
Sung Won Chun <s1chun3636@gmail.com>
2017-02-23 02:06:42 -0800

    colored menu

--- app/src/main/java/com/example/sungwon/messageinabottle/MainActivity.java
+++ app/src/main/java/com/example/sungwon/messageinabottle/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.sungwon.messageinabottle;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView mBottomNav;
@@ -17,5 +19,25 @@ public class MainActivity extends AppCompatActivity {
     private void setupView() {
         mBottomNav = (BottomNavigationView)findViewById(R.id.bottom_navigation);
         BottomNavigationViewHelper.disableShiftMode(mBottomNav);
+        mBottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.action_main_menu:
+                        mBottomNav.setItemBackgroundResource(R.color.orange);
+                        break;
+                    case R.id.action_memory:
+                        mBottomNav.setItemBackgroundResource(R.color.gray);
+                        break;
+                    case R.id.action_send:
+                        mBottomNav.setItemBackgroundResource(R.color.colorPrimary);
+                        break;
+                    case R.id.action_setting:
+                        mBottomNav.setItemBackgroundResource(R.color.gray2);
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
