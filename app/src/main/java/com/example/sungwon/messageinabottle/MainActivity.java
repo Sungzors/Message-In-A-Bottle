f7ab4f4e0b2a4b3a07187f477a051b4d1a4eeeee
Sung Won Chun <s1chun3636@gmail.com>
2017-02-23 01:28:31 -0800

    disabling shift mode

--- app/src/main/java/com/example/sungwon/messageinabottle/MainActivity.java
+++ app/src/main/java/com/example/sungwon/messageinabottle/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.sungwon.messageinabottle;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView mBottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupView();
+    }
+
+    private void setupView() {
+        mBottomNav = (BottomNavigationView)findViewById(R.id.bottom_navigation);
+        BottomNavigationViewHelper.disableShiftMode(mBottomNav);
     }
 }
